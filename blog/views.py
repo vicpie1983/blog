62b92c1757c899e0dacca2b67e2cd55ee230a5e8
vicpie1983 <vicpie1983@gmail.com>
2023-03-26 08:24:21 +0900

    post_list 작성

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(
+        category__is_publish=True, # 공개 카테고리
+        published_date__lte=timezone.now() # 발행된 글
+    ).order_by('-published_date') # 최근 발행글이 가장 앞에 오도록 정렬
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
