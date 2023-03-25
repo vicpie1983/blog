53bf27b808a06cfe24881debc6fa094cd54dd4a2
vicpie1983 <vicpie1983@gmail.com>
2023-03-26 06:35:00 +0900

    Post 모델 추가

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,19 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+class Post(models.Model):
+    category = models.ForeignKey('blog.Category', on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
