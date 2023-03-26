96edf5a6c7b66d140d40da023255c7711abb7178
vicpie1983 <vicpie1983@gmail.com>
2023-03-26 09:37:51 +0900

    html 코드 정리

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Post
+from .models import Post, Category
 
 
 def post_list(request):
+    categorys = Category.objects.filter(is_publish=True).order_by('ordering')
+
     posts = Post.objects.filter(
         category__is_publish=True, # 공개 카테고리
         published_date__lte=timezone.now() # 발행된 글
     ).order_by('-published_date') # 최근 발행글이 가장 앞에 오도록 정렬
-    return render(request, 'blog/post_list.html', {'posts': posts})
+
+    return render(request, 'blog/post_list.html', {'categorys': categorys, 'posts': posts})
 
