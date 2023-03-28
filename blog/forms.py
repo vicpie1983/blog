cd1c28cf268dc84b9bb4b5b668e8bbe16c04a095
vicpie1983 <vicpie1983@gmail.com>
2023-03-28 11:30:32 +0900

    새글작성 폼의 레이블 변경

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('category', 'title', 'text',)
+        labels = {
+            "category": "카테고리",
+            "title": "제목",
+            "text": "내용",
+        }
