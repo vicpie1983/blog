1d47845d6c181208f3505059577651dfdf1f948c
vicpie1983 <vicpie1983@gmail.com>
2023-03-28 12:05:30 +0900

    글수정하기

--- blog/views.py
+++ blog/views.py
@@ -25,11 +25,23 @@ def post_new(request):
             post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
-            print('type:',type(post))
-            print('post.pk:', post.pk)
             post.save()
-            print('post.pk:', post.pk)
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
