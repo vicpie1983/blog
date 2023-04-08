4ae0318f51db2a00362cb5ccf88cf9a2cf481b18
vicpie1983 <vicpie1983@gmail.com>
2023-04-08 15:41:24 +0900

    관리자와 방문자의 댓글이 다르게 보이도록 함

--- blog/models.py
+++ blog/models.py
@@ -43,6 +43,9 @@ class Post(models.Model):
     tags = models.ManyToManyField('blog.Tag', verbose_name='태그')
     series = models.ManyToManyField('blog.Series', verbose_name='시리즈')
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
