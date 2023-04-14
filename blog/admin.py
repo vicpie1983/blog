7d5c3abed96674d3118902bf7aa3707b4fe9bb00
vicpie1983 <vicpie1983@gmail.com>
2023-04-14 21:27:56 +0900

    어드민 Series 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category, Post, Comment
+from .models import Category, Post, Comment, Series
 
 
 admin.site.register(Category)
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Series)
